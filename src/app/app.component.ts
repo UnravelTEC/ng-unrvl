42512b06aabc92bbdaeaef0ff7a16cede132ad72
Michael Maier <michael.maier@mailbox.org>
2018-10-23 17:27:07 +0200

    routing parts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+  title = 'TFrontend';
   prod = environment.production;
   restMsg = '';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,38 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { RouterModule, Routes } from '@angular/router';
+import { SettingsComponent } from './settings/settings.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const appRoutes: Routes = [
+  { path: 'Settings', component: SettingsComponent },
+  { path: 'Dashboard',      component: DashboardComponent },
+/*  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },*/
+  { path: '',
+    redirectTo: 'Dashboard',
+    pathMatch: 'full'
+  },
+  { path: '**', component: DashboardComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
