dcf95d3cf059f51c6e729d86c612d126cd2e5673
Michael Maier <Michael.Maier@unraveltec.com>
2018-11-06 22:55:29 +0100

    titel etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
   }
 
   public constructor(private http: HttpClient, private titleService: Title) {
-    this.setTitle('Good morning Vietnam!');
+    this.setTitle('UnravelTEC');
   }
 
   ngOnInit() {
