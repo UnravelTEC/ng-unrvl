611bd648eceb776b84d4addab57c3620dbccec86
Michael Maier <michael.maier@mailbox.org>
2018-11-06 18:43:03 +0100

    iaq fetch works

--- src/app/ut-apps/iaq/iaq.component.ts
+++ src/app/ut-apps/iaq/iaq.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UtFetchdataService } from '../../ut-fetchdata.service';
 
 @Component({
   selector: 'app-iaq',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iaq.component.css']
 })
 export class IaqComponent implements OnInit {
-
-  constructor() { }
+  constructor(private utFetchdataService: UtFetchdataService) {}
 
   ngOnInit() {
+    this.getData();
   }
 
+  currentData = {
+    timestamp: 0,
+    value: "00"};
+  CO2_value = 0;
+
+   getData() {
+    this.utFetchdataService.getHTTPData().subscribe(
+      (data: Config) =>
+        (this.currentData = {
+          timestamp: data['data']['result'][0]['value'][0] * 1000,
+          value: parseFloat(data['data']['result'][0]['value'][1], 10)
+
+        })
+    );
+  }
 }
---
+++ src/app/ut-fetchdata.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtFetchdataService {
+
+  constructor(private http: HttpClient) { }
+
+  httpURL = 'http://belinda.cgv.tugraz.at:9090/api/v1/query?query=co2{location="FuzzyLab",sensor="scd30"}';
+  Config = {}
+
+  getHTTPData() {
+    return this.http.get(this.httpURL);
+  }
+}
