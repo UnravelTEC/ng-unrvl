8af97a5eeaf4f498fb2ec61ded7e802584cc3e2d
Michael Maier <michael.maier@mailbox.org>
2018-11-09 14:59:30 +0100

    regular data update running

--- src/app/ut-apps/iaq/iaq.component.ts
+++ src/app/ut-apps/iaq/iaq.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UtFetchdataService } from '../../shared/ut-fetchdata.service';
 
+import { interval, Subscription } from 'rxjs';
+
 //import { EventEmitter} from '@angular/core';
 
 @Component({
@@ -9,7 +11,6 @@ import { UtFetchdataService } from '../../shared/ut-fetchdata.service';
   styleUrls: ['./iaq.component.css']
 })
 export class IaqComponent implements OnInit {
-
   /*@Input() item: any;
   @Output() itemChange = new EventEmitter();*/
 
@@ -23,40 +24,41 @@ export class IaqComponent implements OnInit {
 
   ngOnInit() {
     this.getData();
+
+    this.intervalSubscription = interval(1000).subscribe(counter => {
+      this.getData();
+    });
   }
 
-  url = 'http://belinda.cgv.tugraz.at:9090/api/v1/query?query=co2{location="FuzzyLab",sensor="scd30"}';
+  url =
+    'http://belinda.cgv.tugraz.at:9090/api/v1/query?query=co2{location="FuzzyLab",sensor="scd30"}';
+  private requests_underway = 0; //don't flood the server if it is not fast enough
+
+  intervalSubscription: Subscription;
 
   getData() {
-    this.utFetchdataService.getHTTPData(this.url).subscribe(
-      (data: SingleValue) => {
+    //console.log(this.requests_underway);
+    if (this.requests_underway > 0) {
+      console.log('not sending data request, one on the way already');
+      return;
+    }
+    if (this.requests_underway > 5) {
+      console.error('5 requests on the way, none returned, check server conn.');
+      return;
+    }
+    this.requests_underway++;
+    this.utFetchdataService
+      .getHTTPData(this.url)
+      .subscribe((data: SingleValue) => {
+        this.requests_underway--;
         this.currentData = {
           timestamp: data['data']['result'][0]['value'][0] * 1000,
           value: parseFloat(data['data']['result'][0]['value'][1])
         };
-        this.callNext();
-      }
-      //this.saveDataAndCallNext
-      );
+      });
   }
-
-  saveDataAndCallNext(data: SingleValue) {
-    //console.log(data);
-    this.currentData = {
-      timestamp: data['data']['result'][0]['value'][0] * 1000,
-      value: parseFloat(data['data']['result'][0]['value'][1])
-    };
-    /*console.log(this.currentData.value)
-    console.log(this.currentData.timestamp)
-    setTimeout(this.getData, 1000);*/
-  }
-  callNext() {
-    setTimeout(this.getData, 1000);
-  }
-//Problem: update gets not emitted when called via non-arrow-function
-
-
 }
+
 
 export interface SingleValue {
   timestamp: number;
