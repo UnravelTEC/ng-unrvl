33892631c1e4f79173ac52f6732056b12f9ecaf4
Michael Maier <michi@opportunity>
2018-11-14 15:36:43 +0100

    graph now working in submodule

--- src/app/ut-apps/co2-graph/co2-graph.module.ts
+++ src/app/ut-apps/co2-graph/co2-graph.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 import { Co2GraphRoutingModule } from './co2-graph-routing.module';
 import { Co2GraphComponent } from './co2-graph.component';
 
+import { NgDygraphsModule } from 'ng-dygraphs';
+
 @NgModule({
   imports: [
     CommonModule,
-    Co2GraphRoutingModule
+    Co2GraphRoutingModule,
+    NgDygraphsModule
   ],
   declarations: [Co2GraphComponent]
 })
