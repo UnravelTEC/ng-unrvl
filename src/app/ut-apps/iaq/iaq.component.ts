bcf133b9800051d186d225e7871d4ec092621148
Michael Maier <Michael.Maier@unraveltec.com>
2018-11-19 13:14:42 +0100

    arrow funct -> normal funct.

--- src/app/ut-apps/iaq/iaq.component.ts
+++ src/app/ut-apps/iaq/iaq.component.ts
@@ -15,7 +15,7 @@ export class IaqComponent implements OnInit {
   };
   CO2_value = 0;
 
-  constructor(private utFetchdataService: UtFetchdataService) {}
+  constructor(private utFetchdataService: UtFetchdataService) { }
 
   ngOnInit() {
     this.getData();
@@ -44,15 +44,19 @@ export class IaqComponent implements OnInit {
     this.requests_underway++;
     this.utFetchdataService
       .getHTTPData(this.url)
-      .subscribe((data: SingleValue) => {
-        this.requests_underway--;
-        this.currentData = {
-          timestamp: data['data']['result'][0]['value'][0] * 1000,
-          value: parseFloat(data['data']['result'][0]['value'][1])
-        };
-      });
+      .subscribe((data: SingleValue) => this.loadData(data))
+  }
+
+  loadData(data: SingleValue) {
+    this.requests_underway--;
+    this.currentData = {
+      timestamp: data['data']['result'][0]['value'][0] * 1000,
+      value: parseFloat(data['data']['result'][0]['value'][1])
+    };
   }
 }
+
+
 
 export interface SingleValue {
   timestamp: number;
