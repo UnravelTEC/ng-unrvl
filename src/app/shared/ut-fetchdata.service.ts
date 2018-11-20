141306997e1f46e7b34a9ca5ccdcec297ad8c883
Michael Maier <Michael.Maier@unraveltec.com>
2018-11-20 09:42:25 +0100

    first query into dygraph

--- src/app/shared/ut-fetchdata.service.ts
+++ src/app/shared/ut-fetchdata.service.ts
@@ -5,14 +5,26 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class UtFetchdataService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   httpURL =
     'http://belinda.cgv.tugraz.at:9090/api/v1/query?query=co2{location="FuzzyLab",sensor="scd30"}';
   Config = {};
 
-  getHTTPData(url : string) {
+  queryEndPoint = "http://belinda.cgv.tugraz.at:9090/api/v1/";
+  queryDefaultStep: number = 1000; //ms
+
+  getHTTPData(url: string) {
     var thisurl = (url) ? url : this.httpURL;
     return this.http.get(thisurl);
+  }
+
+  private buildRangeQuery(queryEndPoint: string, queryString: string, start: Date, end: Date, step) {
+    return queryEndPoint + "query_range?query=" + queryString + "&start=" + start.toISOString() + "&end=" + end.toISOString() + "&step=" + step + "ms";
+  }
+
+  getRange(queryString: string, start: Date, end: Date, step = this.queryDefaultStep, queryEndPoint = this.queryEndPoint) {
+
+    return this.http.get(this.buildRangeQuery(queryEndPoint, queryString, start, end, step));
   }
 }
--- src/app/ut-apps/co2-graph/co2-graph.component.ts
+++ src/app/ut-apps/co2-graph/co2-graph.component.ts
@@ -2,24 +2,31 @@ import { Component, OnInit } from '@angular/core';
 
 import { NgDygraphsModule } from 'ng-dygraphs';
 
+import { UtFetchdataService } from '../../shared/ut-fetchdata.service';
+
 @Component({
   selector: 'app-co2-graph',
   templateUrl: './co2-graph.component.html',
   styleUrls: ['./co2-graph.component.css']
 })
 export class Co2GraphComponent implements OnInit {
-  constructor() {}
+  constructor(private utFetchdataService: UtFetchdataService) { }
+
+  queryString = 'co2{location="FuzzyLab",sensor="scd30"}'
 
   data = [];
 
   options = {};
 
   ngOnInit() {
+    console.log('calling http');
+
     this.data = [
       [new Date('2008/05/07'), 75],
       [new Date('2008/05/08'), 70],
       [new Date('2008/05/09'), 80]
     ];
+    console.log(this.data);
     this.options = {
       width: '750',
       height: '350',
@@ -30,5 +37,31 @@ export class Co2GraphComponent implements OnInit {
       animatedZooms: true,
       pointSize: 4
     };
+
+    let starttime = new Date();
+    let endtime = new Date();
+    starttime.setSeconds(endtime.getSeconds() - 600);
+
+
+    this.utFetchdataService
+      .getRange(this.queryString, starttime, endtime, 3000)
+      .subscribe((data: Object) => this.handleReceivedData(data));
+  }
+
+  handleReceivedData(data: Object) {
+    console.log('received Data:');
+    console.log(data);
+    let metric = data['data']['result'][0]['metric'];
+    let values = data['data']['result'][0]['values']
+    this.data = [];
+    console.log(values);
+    values.forEach(element => {
+      this.data.push(
+        [
+          new Date(element[0] * 1000),
+          Number(element[1])
+        ])
+    });
+    console.log(this.data);
   }
 }
