07ecbc25767a7124c500ebf0670705e8cc2a8ad9
Michael Maier <michi@opportunity>
2018-12-08 21:31:43 +0100

    set and edit of times

--- src/app/ut-apps/weihnachtsvorlesung/annotations-editor/annotations-editor.component.ts
+++ src/app/ut-apps/weihnachtsvorlesung/annotations-editor/annotations-editor.component.ts
@@ -23,6 +23,12 @@ export class AnnotationsEditorComponent implements OnInit {
   @Output()
   triggerChange = new EventEmitter<number>();
 
+  edit = {
+    x: false,
+    clapStart: false,
+    clapEnd: false
+  };
+
   constructor() {}
 
   ngOnInit() {}
@@ -32,26 +38,31 @@ export class AnnotationsEditorComponent implements OnInit {
     this.triggerChange.emit(1);
   }
 
-  adjustTime(secondsToAdjust: number) {
-    this.currentAnnotation.x += secondsToAdjust * 1000; // x is in ms
+  adjustTime(key: string, secondsToAdjust: number) {
+    this.currentAnnotation[key] += secondsToAdjust * 1000; // x is in ms
+
     const index = this.currentAnnotation['shortText'];
     this.annotationList2.forEach(element => {
-      if(element['shortText'] == index) {
-        element['x'] += secondsToAdjust * 1000;
+      if (element['shortText'] == index) {
+        element[key] += secondsToAdjust * 1000;
       }
     });
-
   }
   toggleDisplay() {
-    this.expanded = ! this.expanded;
+    this.expanded = !this.expanded;
+  }
+  set(which: string) {
+    this.currentAnnotation[which] = new Date().valueOf();
+  }
+  show(which:string) {
+    this.edit[which] = !this.edit[which];
   }
   delete() {
     for (let i = 0; i < this.annotationList.length; i++) {
-      if(this.annotationList[i]['x'] == this.currentAnnotation['x']) {
-        this.annotationList.splice(i,1);
-        this.annotationList2.splice(i,1);
+      if (this.annotationList[i]['x'] == this.currentAnnotation['x']) {
+        this.annotationList.splice(i, 1);
+        this.annotationList2.splice(i, 1);
       }
     }
-
   }
 }
