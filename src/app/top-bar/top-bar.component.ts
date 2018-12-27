9c453eea8b4c49f78463497af4f2da7e1da38b6f
Michael Maier <michi@opportunity>
2018-12-27 21:09:36 +0100

    top bar update

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { interval, Subscription } from 'rxjs';
+
+import { HelperFunctionsService } from '../core/helper-functions.service';
+
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
@@ -13,9 +17,20 @@ export class TopBarComponent implements OnInit {
   @Input()
   appName = '/';
 
-  constructor() { }
+  public currentTime: Date;
+  public hostName: string;
+
+  private intervalSubscription: Subscription;
+
+  constructor(private h: HelperFunctionsService) { }
 
   ngOnInit() {
+    this.currentTime = new Date();
+    this.hostName = this.h.getBaseURL().replace(/http[s]?:\/\//,'');
+
+    this.intervalSubscription = interval(60000).subscribe(counter => {
+      this.currentTime = new Date();
+    });
   }
 
 }
