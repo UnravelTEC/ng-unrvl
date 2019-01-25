cc936ef089ed833fe5d290e5fbe737b3a08fe3bc
Michael Maier <Michael.Maier@unraveltec.com>
2019-01-25 15:41:38 +0100

    linting

--- src/app/ut-apps/loudness/loudness-routing.module.ts
+++ src/app/ut-apps/loudness/loudness-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoudnessComponent } from './loudness.component';const routes: Routes = [ { path: '', component: LoudnessComponent }];
+import { LoudnessComponent } from './loudness.component';
+const routes: Routes = [{ path: '', component: LoudnessComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class LoudnessRoutingModule { }
+export class LoudnessRoutingModule {}
