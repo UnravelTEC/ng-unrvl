ac3fe4fcf303ce8300b123be2671f084dfbc0efd
Michael Maier <Michael.Maier@unraveltec.com>
2019-02-12 18:37:05 +0100

    csvexport visible range - still some bugs to fix

--- src/app/shared/ut-dygraph/ut-dygraph.component.ts
+++ src/app/shared/ut-dygraph/ut-dygraph.component.ts
@@ -18,6 +18,7 @@ import * as FileSaver from 'file-saver';
 import cloneDeep from 'lodash-es/cloneDeep';
 import { HttpClient } from '@angular/common/http';
 import { formatDate } from '@angular/common';
+import { tick } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-ut-dygraph',
@@ -1283,6 +1284,63 @@ export class UtDygraphComponent implements OnInit, OnDestroy {
     });
   }
 
+  returnDataRange(indata, from: Date, to: Date) {
+    if(!indata.length || !from || !to) {
+      console.error('returnDataRange: empty input',indata,from,to);
+      return [];
+    }
+
+
+    function isbetweenDate(target: Date, lower: Date, upper: Date) {
+      return (
+        lower.valueOf() <= target.valueOf() &&
+        upper.valueOf() >= target.valueOf()
+      );
+    }
+
+    console.log('slicing from', from, 'to', to, 'in', indata);
+    let startindex = undefined;
+    if(from.valueOf() < indata[0][0].valueOf()) {
+      startindex = 0
+    }
+    let endindex = undefined;
+    if(to.valueOf() > indata[indata.length-1][0].valueOf()) {
+      endindex = indata.length -1;
+    }
+
+    for (let i = 0; i < indata.length - 1; i++) {
+      const elementDate = indata[i][0];
+      const nextElement = indata[i + 1][0];
+      if (startindex === undefined) {
+        // if(i === 0) {
+        //   console.log(elementDate.valueOf(), from.valueOf())
+        // }
+        if (isbetweenDate(from, elementDate, nextElement)) {
+          startindex = i;
+          continue;
+        }
+      }
+      if (endindex === undefined) {
+        if (isbetweenDate(to, elementDate, nextElement)) {
+          endindex = i;
+          break;
+        }
+      }
+    }
+    if (!startindex) {
+      console.error('startindex not found');
+      return [];
+    }
+    if (!endindex) {
+      endindex = indata.length;
+    }
+    const outdata = indata.slice(startindex, endindex + 1);
+
+    console.log('dataslice from', startindex, 'to', endindex);
+
+    return outdata;
+  }
+
   exportCSV(data?) {
     //header
     const separator = '\t';
@@ -1290,7 +1348,11 @@ export class UtDygraphComponent implements OnInit, OnDestroy {
 
     const labels = this.dyGraphOptions['labels'];
     if (!data) {
-      data = this.displayedData;
+      data = this.returnDataRange(
+        this.displayedData,
+        this.fromZoom,
+        this.toZoom
+      );
     }
     let header = '';
     if (labels.length === 0 || data.length === 0) {
