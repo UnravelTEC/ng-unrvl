0d47a68593c5d09f92b35c50ec4c003366fa7361
Michael Maier <michael.maier@unraveltec.com>
2019-03-24 13:34:43 +0100

    temperatures

--- src/app/ut-apps/temperatures/temperatures.component.ts
+++ src/app/ut-apps/temperatures/temperatures.component.ts
@@ -8,8 +8,20 @@ import { GlobalSettingsService } from 'app/core/global-settings.service';
 })
 export class TemperaturesComponent implements OnInit {
   constructor(private globalSettings: GlobalSettingsService) {
-    this.globalSettings.emitChange({ appName: 'temperatures' });
+    this.globalSettings.emitChange({ appName: 'Temperatures Overview' });
   }
+
+  fetchFromServerIntervalMS = 1000;
+
+  graphstyle = {
+    position: 'absolute',
+    top: '1vh',
+    bottom: '1vh',
+    left: '1vw',
+    right: '1vw'
+  };
+  dataSeriesLabels = ['Temperature'];
+  startTime = '15m';
 
   ngOnInit() {}
 }
