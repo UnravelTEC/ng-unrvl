25e8646c3f72e293cd1a9418ab8d1c5e5c09c78d
Michael Maier <michael.maier@unraveltec.com>
2019-06-07 07:54:00 +0200

    settings section: allow load pre-stored server settings

--- src/app/ut-settings/settings-panel/settings-panel.component.ts
+++ src/app/ut-settings/settings-panel/settings-panel.component.ts
@@ -17,34 +17,159 @@ export class SettingsPanelComponent implements OnInit {
         title: 'Server Settings'
       },
       settings: {
+        serverName: {
+          fieldName: 'Endpoint Name',
+          fieldValue: ''
+        },
         serverHostName: {
           fieldName: 'Server hostname/ip',
           fieldValue: ''
         },
-        prometheusPort: { fieldName: 'Prometheus port', fieldValue: '' }, // 9090
+        prometheusPort: { fieldName: 'Prometheus port', fieldValue: '80' }, // 9090
         prometheusPath: {
           fieldName: 'Prometheus database API path',
           fieldValue: 'prometheus/api/v1/'
         },
         prometheusProtocol: {
           fieldName: 'Prometheus Protocol',
-          fieldValue: ''
+          fieldValue: 'http'
         },
         apiPort: {
           fieldName: 'API port',
-          fieldValue: ''
+          fieldValue: '80'
         },
         apiPath: {
           fieldName: 'API path',
-          fieldValue: '' // 'api/'
+          fieldValue: 'api/'
         },
         apiProtocol: {
           fieldName: 'API protocol',
-          fieldValue: ''
+          fieldValue: 'http'
         }
       }
     }
   };
+
+  settingsArray = {
+    newton: {
+      server: {
+        // settingsSection
+        settingAttributes: {
+          title: 'Server Settings'
+        },
+        settings: {
+          serverName: {
+            fieldName: 'Endpoint Name',
+            fieldValue: 'UnravelTEC Demo Server'
+          },
+          serverHostName: {
+            fieldName: 'Server hostname/ip',
+            fieldValue: 'scpunraveltec2.tugraz.at'
+          },
+          prometheusPort: { fieldName: 'Prometheus port', fieldValue: '443' }, // 9090
+          prometheusPath: {
+            fieldName: 'Prometheus database API path',
+            fieldValue: 'prometheus/api/v1/'
+          },
+          prometheusProtocol: {
+            fieldName: 'Prometheus Protocol',
+            fieldValue: 'https'
+          },
+          apiPort: {
+            fieldName: 'API port',
+            fieldValue: '443'
+          },
+          apiPath: {
+            fieldName: 'API path',
+            fieldValue: 'api/'
+          },
+          apiProtocol: {
+            fieldName: 'API protocol',
+            fieldValue: 'https'
+          }
+        }
+      }
+    },
+    localhost: {
+      server: {
+        // settingsSection
+        settingAttributes: {
+          title: 'Server Settings'
+        },
+        settings: {
+          serverName: {
+            fieldName: 'Endpoint Name',
+            fieldValue: 'LocalHost'
+          },
+          serverHostName: {
+            fieldName: 'Server hostname/ip',
+            fieldValue: 'localhost'
+          },
+          prometheusPort: { fieldName: 'Prometheus port', fieldValue: '8ß' }, // 9090
+          prometheusPath: {
+            fieldName: 'Prometheus database API path',
+            fieldValue: 'prometheus/api/v1/'
+          },
+          prometheusProtocol: {
+            fieldName: 'Prometheus Protocol',
+            fieldValue: 'http'
+          },
+          apiPort: {
+            fieldName: 'API port',
+            fieldValue: '80'
+          },
+          apiPath: {
+            fieldName: 'API path',
+            fieldValue: 'api/'
+          },
+          apiProtocol: {
+            fieldName: 'API protocol',
+            fieldValue: 'http'
+          }
+        }
+      }
+    },
+    default: {
+      server: {
+        // settingsSection
+        settingAttributes: {
+          title: 'Server Settings'
+        },
+        settings: {
+          serverName: {
+            fieldName: 'Endpoint Name',
+            fieldValue: 'Default Host connection'
+          },
+          serverHostName: {
+            fieldName: 'Server hostname/ip',
+            fieldValue: '$baseurl'
+          },
+          prometheusPort: { fieldName: 'Prometheus port', fieldValue: '80' }, // 9090
+          prometheusPath: {
+            fieldName: 'Prometheus database API path',
+            fieldValue: 'prometheus/api/v1/'
+          },
+          prometheusProtocol: {
+            fieldName: 'Prometheus Protocol',
+            fieldValue: 'http'
+          },
+          apiPort: {
+            fieldName: 'API port',
+            fieldValue: '80'
+          },
+          apiPath: {
+            fieldName: 'API path',
+            fieldValue: 'api/'
+          },
+          apiProtocol: {
+            fieldName: 'API protocol',
+            fieldValue: 'http'
+          }
+        }
+      }
+    }
+  };
+  endpointValue = undefined;
 
   globalSettings = {};
   localStoredSettings = false;
@@ -98,6 +223,24 @@ export class SettingsPanelComponent implements OnInit {
       this.globalSettingsUnsaved = this.localStorage.get('globalSettings');
       this.localStoredSettings = true;
     }
+  }
+
+  loadEndpoint() {
+    if (!this.endpointValue) {
+      alert('select an Endpoint');
+    }
+    const loadedSettings = this.settingsArray[this.endpointValue];
+    console.log(loadedSettings);
+
+    this.globalSettingsUnsaved = loadedSettings;
+
+    this.localStorage.set('globalSettings', this.globalSettingsUnsaved);
+    this.globalSettings = JSON.parse(
+      JSON.stringify(this.globalSettingsUnsaved)
+    );
+
+    this.globalSettingsService.reloadSettings();
+    this.localStoredSettings = true;
   }
 
   save() {
--- src/app/ut-settings/ut-settings.module.ts
+++ src/app/ut-settings/ut-settings.module.ts
@@ -5,15 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { UtSettingsRoutingModule } from './ut-settings-routing.module';
 import { SettingsPanelComponent } from './settings-panel/settings-panel.component';
 import { DisplayBrighnessComponent } from './pieces/display-brighness/display-brighness.component';
-import {MatSliderModule} from '@angular/material/slider';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   imports: [
     CommonModule,
     UtSettingsRoutingModule,
     FormsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatSelectModule
   ],
   declarations: [SettingsPanelComponent, DisplayBrighnessComponent]
 })
-export class UtSettingsModule { }
+export class UtSettingsModule {}
