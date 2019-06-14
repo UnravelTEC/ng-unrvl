983dadef0cb6a8b257359e544a20e1088a50be93
Michael Maier <Michael.Maier@unraveltec.com>
2019-06-14 16:12:48 +0200

    system service table displays services from api

--- src/app/ut-settings/services/services.component.ts
+++ src/app/ut-settings/services/services.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalSettingsService } from '../../core/global-settings.service';
+import { UtFetchdataService } from '../../shared/ut-fetchdata.service';
 
 @Component({
   selector: 'app-services',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.scss']
 })
 export class ServicesComponent implements OnInit {
+  services = [];
 
-  constructor() { }
-
+  constructor(
+    private utHTTP: UtFetchdataService,
+    private globalSettings: GlobalSettingsService
+  ) {
+    this.globalSettings.emitChange({ appName: 'services' });
+  }
   ngOnInit() {
+    if (this.globalSettings.getAPIEndpoint()) {
+      this.getServices();
+    }
   }
 
+  getServices() {
+    this.utHTTP
+      .getHTTPData(this.globalSettings.getAPIEndpoint() + 'system/services.php')
+      .subscribe((data: Object) => this.acceptServices(data));
+  }
+  acceptServices(data: Object) {
+    console.log('services:', data);
+    if (data && data['services']) {
+      this.services = data['services'];
+    }
+  }
 }
