14cf8d8114f29e019c368ce6eeaf57000feb93de
Michael Maier <Michael.Maier@unraveltec.com>
2019-06-18 12:42:39 +0200

    rename to w/o spelling error

--- src/app/ut-settings/ut-settings.module.ts
+++ src/app/ut-settings/ut-settings.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { UtSettingsRoutingModule } from './ut-settings-routing.module';
 import { SettingsPanelComponent } from './settings-panel/settings-panel.component';
-import { DisplayBrighnessComponent } from './pieces/display-brighness/display-brighness.component';
+import { DisplayBrighnessComponent } from './pieces/display-brightness/display-brightness.component';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSelectModule } from '@angular/material/select';
 import { SystemTimeComponent } from './pieces/system-time/system-time.component';
