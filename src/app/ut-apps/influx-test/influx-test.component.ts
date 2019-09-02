69681610f95bcc834a995e582cd522237e812d38
Michael Maier <Michael.Maier@unraveltec.com>
2019-09-02 20:48:59 +0200

    MQTT receive works and summes up values in a table

--- src/app/ut-apps/influx-test/influx-test.component.ts
+++ src/app/ut-apps/influx-test/influx-test.component.ts
@@ -12,10 +12,37 @@ export class InfluxTestComponent implements OnInit {
   private client;
   clientID = 'clientID_' + String(Math.random() * 100);
 
+  public mqttMessages = 'empty';
+
+  public sensorData = {};
+  public sensorDataExample = {
+    myBME: {
+      temperature_degC: {
+        index: {
+          value: 25.5,
+          tags: { id: '0x77' }
+        }
+      },
+
+      pressure_hPA: {
+        index: {
+          value: 900,
+          tags: { id: '0x77' }
+        }
+      },
+      humidity_rel_percent: {
+        index: {
+          value: 42,
+          tags: { id: '0x77' }
+        }
+      }
+    }
+  };
+
   constructor(
     private globalSettings: GlobalSettingsService,
-    private utHTTP: UtFetchdataService,
-    ) {
+    private utHTTP: UtFetchdataService
+  ) {
     this.globalSettings.emitChange({ appName: 'influx-test' });
   }
   ngOnInit() {
@@ -40,12 +67,12 @@ export class InfluxTestComponent implements OnInit {
     this.client.onConnectionLost = this.onConnectionLost;
     this.client.onMessageArrived = this.onMessageArrived;
     document['MQTT_CLIENT'] = this.client;
-    console.log('onInit',this.client);
+    document['MQTT_CLIENT']['father'] = this;
+    console.log('onInit', this.client);
     this.client.connect({
       onSuccess: this.onConnect,
       onFailure: this.onFailure
     });
-
   }
 
   printResult(data: Object) {
@@ -60,19 +87,40 @@ export class InfluxTestComponent implements OnInit {
   }
 
   onMessageArrived(message: Object) {
-    console.log('got MQTT message');
-    console.log(message);
-    const payload = JSON.parse(message['payloadString'])
-    console.log(payload);
+    const father = document['MQTT_CLIENT']['father'];
 
+    const arr = message['topic'].split('/');
+    const sensor = arr[2];
+    const metric = arr[3];
+    // console.log('got MQTT message from sensor ', sensor, ' about ', metric);
+    try {
+      const payload = JSON.parse(message['payloadString']);
+      const value = payload['value'];
+      let tags = JSON.parse(message['payloadString']);
+      delete tags['value'];
+      const index = JSON.stringify(tags);
+
+      // console.log(payload);
+      if (!father.sensorData[sensor]) {
+        father.sensorData[sensor] = {};
+      }
+      if(!father.sensorData[sensor][metric]) {
+        father.sensorData[sensor][metric] = {}
+      }
+      father.sensorData[sensor][metric][index] = { value: value, tags: tags };
+    } catch (err) {
+      console.error(err);
+    }
   }
+
   onFailure(message) {
     console.error('MQTT failure on connect');
     console.error(message);
   }
   onConnectionLost(responseObject) {
+    console.error('onConnectionLost object: ', responseObject);
     if (responseObject.errorCode !== 0) {
-      console.error('onConnectionLost:' + responseObject.errorMessage);
+      console.error('onConnectionLost:', responseObject.errorMessage);
     }
   }
 }
