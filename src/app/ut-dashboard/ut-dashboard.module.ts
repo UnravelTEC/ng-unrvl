07650e87acb8a68028a548be909c9c4fdbbab104
Michael Maier <michael.maier@unraveltec.com>
2019-09-02 21:58:07 +0200

    bn links

--- src/app/ut-dashboard/ut-dashboard.module.ts
+++ src/app/ut-dashboard/ut-dashboard.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { UtDashboardRoutingModule } from './ut-dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UtDashboardTileComponent } from './ut-dashboard-tile/ut-dashboard-tile.component';
+import { BrightnessModule } from '../ut-apps/tiles/brightness/brightness.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    UtDashboardRoutingModule
+    UtDashboardRoutingModule,
+    BrightnessModule
   ],
   declarations: [DashboardComponent, UtDashboardTileComponent]
 })
