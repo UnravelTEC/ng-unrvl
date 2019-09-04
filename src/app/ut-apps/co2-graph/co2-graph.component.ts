4510c280487e9eaf00b44586f037c7f7c963b357
Michael Maier <Michael.Maier@unraveltec.com>
2019-09-04 16:07:30 +0200

    co2 colors

--- src/app/ut-apps/co2-graph/co2-graph.component.ts
+++ src/app/ut-apps/co2-graph/co2-graph.component.ts
@@ -22,8 +22,8 @@ export class Co2GraphComponent implements OnInit, OnDestroy {
     [0.01, 'white'], // first one not used
     [415, 'rgba(0, 128, 0, 0.678)'], // green
     [600,'rgba(0, 128, 0, 0.35)'],
-    [800, 'rgba(255, 255, 0, 0.35)'], // yellow
-    [1200, 'rgba(255, 166, 0, 0.35)'], // orange
+    [1000, 'rgba(255, 255, 0, 0.35)'], // yellow
+    [1500, 'rgba(255, 166, 0, 0.35)'], // orange
     [20000, 'rgba(255, 0, 0, 0.35)'] // red
   ];
 
