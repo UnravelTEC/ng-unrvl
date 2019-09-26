eec1ba1af9ca95adbb276ff2605d50c9e7c16994
Michael Maier <Michael.Maier@unraveltec.com>
2019-09-26 08:57:40 +0200

    c'out stuff for local ip addr discovery

--- src/app/core/global-settings.service.ts
+++ src/app/core/global-settings.service.ts
@@ -83,32 +83,32 @@ export class GlobalSettingsService implements OnInit {
   ngOnInit() {
     this.reloadSettings();
 
-    if (this.RTCPeerConnection) {
-      var rtc = new RTCPeerConnection({ iceServers: [] });
-      if (1 || window['mozRTCPeerConnection']) {
-        // FF [and now Chrome!] needs a channel/stream to proceed
-        rtc.createDataChannel('', { reliable: false });
-      }
+    // if (this.RTCPeerConnection) {
+    //   var rtc = new RTCPeerConnection({ iceServers: [] });
+    //   if (1 || window['mozRTCPeerConnection']) {
+    //     // FF [and now Chrome!] needs a channel/stream to proceed
+    //     rtc.createDataChannel('', { reliable: false });
+    //   }
 
-      rtc.onicecandidate = function(evt) {
-        // convert the candidate to SDP so we can run it through our general parser
-        // see https://twitter.com/lancestout/status/525796175425720320 for details
-        if (evt.candidate) {this.grepSDP('a=' + evt.candidate.candidate)};
-      };
-      rtc.createOffer(
-        function(offerDesc) {
-          this.grepSDP(offerDesc.sdp);
-          rtc.setLocalDescription(offerDesc);
-        },
-        function(e) {
-          console.warn('offer failed', e);
-        }
-      );
-    } else {
-      console.error(
-        'error in getLocalIPs: RTCPeerConnection could not be created'
-      );
-    }
+    //   rtc.onicecandidate = function(evt) {
+    //     // convert the candidate to SDP so we can run it through our general parser
+    //     // see https://twitter.com/lancestout/status/525796175425720320 for details
+    //     if (evt.candidate) {this.grepSDP('a=' + evt.candidate.candidate)};
+    //   };
+    //   rtc.createOffer(
+    //     function(offerDesc) {
+    //       this.grepSDP(offerDesc.sdp);
+    //       rtc.setLocalDescription(offerDesc);
+    //     },
+    //     function(e) {
+    //       console.warn('offer failed', e);
+    //     }
+    //   );
+    // } else {
+    //   console.error(
+    //     'error in getLocalIPs: RTCPeerConnection could not be created'
+    //   );
+    // }
   }
 
   grepSDP(sdp) {
