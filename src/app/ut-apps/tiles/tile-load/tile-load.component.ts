ecd2ab51b0efce66e4f05b4a0e4225bef6aa00b3
Michael Maier <Michael.Maier@unraveltec.com>
2019-10-07 18:42:46 +0200

    load tile bg

--- src/app/ut-apps/tiles/tile-load/tile-load.component.ts
+++ src/app/ut-apps/tiles/tile-load/tile-load.component.ts
@@ -7,7 +7,7 @@ import cloneDeep from 'lodash-es/cloneDeep';
 @Component({
   selector: 'app-tile-load',
   templateUrl: './tile-load.component.html',
-  styleUrls: ['./tile-load.component.scss','../tiles.scss']
+  styleUrls: ['./tile-load.component.scss', '../tiles.scss']
 })
 export class TileLoadComponent implements OnInit, OnDestroy {
   @Input()
@@ -27,23 +27,29 @@ export class TileLoadComponent implements OnInit, OnDestroy {
   private mqttRequest = {
     topic: '+/system',
     tagFilters: undefined,
-    valueFilters: ['system_load']
+    valueFilters: ['system_load'],
+    callBack: (obj: Object) => this.updateLoad(obj)
   };
+  private maxDataLength = 300;
 
   highlights =
     '[ {"from": 0, "to": 1, "color": "green"}, \
        {"from": 1, "to": 3, "color": "yellow"}, \
        {"from": 3, "to": 5, "color": "orange"}, \
        {"from": 5, "to": 12, "color": "red"} ]';
+  backGroundLevels = [
+    // the color acts for "everything below $value"
+    [0.01, 'white'], // first one not used
+    [1, 'rgba(0, 128, 0, 0.678)'], // green
+    [3, 'rgba(255, 255, 0, 0.35)'], // yellow
+    [5, 'rgba(255, 166, 0, 0.35)'], // orange
+    [200, 'rgba(255, 0, 0, 0.35)'] // red
+  ];
 
   constructor(private mqtt: MqttService) {}
 
   ngOnInit() {
-    const topic = this.mqttRequest.topic;
-    this.mqtt.request(this.mqttRequest);
-    this.mqttSubscription$ = this.mqtt.observableTopics$[topic].subscribe(
-      (obj: Object) => this.updateLoad(obj)
-    );
+    this.mqttSubscription$ = this.mqtt.request(this.mqttRequest);
     this.triggerChange();
   }
   ngOnDestroy() {
@@ -64,15 +70,17 @@ export class TileLoadComponent implements OnInit, OnDestroy {
         this.dygData.length === 2 &&
         this.dygData[0][1] === 0 &&
         this.dygData[1][1] === 1
-
       ) {
-        console.log('initializing')
+        console.log('initializing');
         this.dygData = [
           [new Date(msg['UTS'] * 1000 - 3000), msg['system_load']],
           newRow
         ];
       } else {
         this.dygData.push(newRow);
+      }
+      if (this.dygData.length > this.maxDataLength) {
+        this.dygData.shift();
       }
       // console.log(cloneDeep(this.dygData));
 
