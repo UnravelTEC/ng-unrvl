60c19bfd7fc3a6c6fb77979b0dd1f0cf8b662c1a
Michael Maier <Michael.Maier@unraveltec.com>
2019-10-07 20:43:30 +0200

    co2 tile to mqtt

--- src/app/ut-apps/tiles/tile-co2/tile-co2.component.ts
+++ src/app/ut-apps/tiles/tile-co2/tile-co2.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { colors } from '../../../shared/colors';
+import { MqttService } from '../../../core/mqtt.service';
+import { HelperFunctionsService } from '../../../core/helper-functions.service';
 
 @Component({
   selector: 'app-tile-co2',
   templateUrl: './tile-co2.component.html',
-  styleUrls: ['./tile-co2.component.scss','../tiles.scss']
+  styleUrls: ['./tile-co2.component.scss', '../tiles.scss']
 })
 export class TileCo2Component implements OnInit {
   public value: number;
@@ -30,8 +32,56 @@ export class TileCo2Component implements OnInit {
     [1500, colors.bg.orange],
     [20000, colors.bg.red]
   ];
+  private mqttRequest = {
+    topic: '+/sensors/SCD30/gas',
+    tagFilters: undefined,
+    valueFilters: ['CO2_ppm'],
+    callBack: (obj: Object) => this.update(obj)
+  };
 
-  constructor() {}
+  changeTrigger = false;
+  triggerChange() {
+    this.changeTrigger = !this.changeTrigger;
+  }
 
-  ngOnInit() {}
+  constructor(private mqtt: MqttService, private h: HelperFunctionsService) {}
+
+  ngOnInit() {
+    this.mqtt.request(this.mqttRequest);
+    this.triggerChange();
+  }
+
+  update(msg: Object) {
+    this.value = this.h.getDeep(msg, [
+      'values',
+      this.mqttRequest.valueFilters[0]
+    ]);
+    if (this.value) {
+      this.cleanInitValues();
+      this.dygData.push([new Date(msg['UTS'] * 1000), this.value]);
+      this.preventTooLargeGrowth();
+    }
+    this.triggerChange();
+  }
+
+  dygLabels = ['Date', 'CO₂'];
+
+  private initDataValue = 0.042;
+  dygData = [
+    [new Date(new Date().valueOf() - 1), this.initDataValue],
+    [new Date(), this.initDataValue]
+  ];
+  cleanInitValues() {
+    if (
+      this.dygData.length === 2 &&
+      this.dygData[0][1] === this.initDataValue
+    ) {
+      this.dygData.shift();
+    }
+  }
+  preventTooLargeGrowth() {
+    while (this.dygData.length > 300) {
+      this.dygData.shift();
+    }
+  }
 }
--- src/app/ut-apps/tiles/tile-co2/tile-co2.module.ts
+++ src/app/ut-apps/tiles/tile-co2/tile-co2.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { TileCo2RoutingModule } from './tile-co2-routing.module';
 import { TileCo2Component } from './tile-co2.component';
-import { UtDygraphModule } from '../../../shared/ut-dygraph/ut-dygraph.module';
+import { UtDygraphInModule } from '../../../shared/ut-dygraph-in/ut-dygraph-in.module';
 import { UtBargaugeMinModule } from '../../../shared/ut-bargauge-min/ut-bargauge-min.module';
 
 @NgModule({
   imports: [
     CommonModule,
     TileCo2RoutingModule,
-    UtDygraphModule,
+    UtDygraphInModule,
     UtBargaugeMinModule
   ],
   declarations: [TileCo2Component],
