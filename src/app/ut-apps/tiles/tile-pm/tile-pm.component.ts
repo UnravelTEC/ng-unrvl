b5a22c7439d8f9a9e018f391f3b92972f9f51191
Michael Maier <Michael.Maier@unraveltec.com>
2019-10-08 16:06:13 +0200

    pm tile via MQTT

--- src/app/ut-apps/tiles/tile-pm/tile-pm.component.ts
+++ src/app/ut-apps/tiles/tile-pm/tile-pm.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MqttService } from '../../../core/mqtt.service';
+import { HelperFunctionsService } from '../../../core/helper-functions.service';
 
 @Component({
   selector: 'app-tile-pm',
@@ -6,19 +8,20 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['../tiles.scss', './tile-pm.component.scss']
 })
 export class TilePmComponent implements OnInit {
-  public value: number;
-
   @Input()
   height = 100;
 
   @Input()
   width = 100;
 
-  extraDyGraphConfig = {
-    // ppm0.5,ppm1,ppm10,ppm2.5,ppm4,pm1,pm10,pm2.5,pm4
-    visibility: [false, false, false, false, false, true, true, true, true]
-  };
   public pm10 = 0;
+  public particle_values = {
+    '10': 0,
+    '4': 0,
+    '2.5': 0,
+    '1': 0,
+    '0.5': 0
+  };
 
   highlights =
     '[ {"from": 0, "to": 25, "color": "green"}, \
@@ -35,10 +38,69 @@ export class TilePmComponent implements OnInit {
     [150, 'rgba(255, 166, 0, 0.35)'], // orange
     [200, 'rgba(255, 0, 0, 0.35)'] // red
   ];
+  private mqttRequest = {
+    topic: '+/sensors/SPS30/+',
+    tagFilters: undefined,
+    valueFilters: [
+      'p10_ugpm3',
+      'p10_ppcm3',
+      'p4_ppcm3',
+      'p2.5_ppcm3',
+      'p1_ppcm3',
+      'p0.5_ppcm3'
+    ],
+    callBack: (obj: Object) => this.update(obj)
+  };
 
-  constructor() {}
+  changeTrigger = false;
+  triggerChange() {
+    this.changeTrigger = !this.changeTrigger;
+  }
 
-  ngOnInit() {}
+  constructor(private mqtt: MqttService, private h: HelperFunctionsService) {}
+
+  ngOnInit() {
+    this.mqtt.request(this.mqttRequest);
+    this.triggerChange();
+  }
+
+  update(msg: Object) {
+    const values = this.h.getDeep(msg, ['values']);
+    if (values) {
+      this.pm10 = values['p10_ugpm3'];
+      this.cleanInitValues();
+      this.dygData.push([new Date(msg['UTS'] * 1000), this.pm10]);
+      for( var key in this.particle_values ) {
+        const values_key = 'p' + key + '_ppcm3';
+        if(values[values_key]) {
+          this.particle_values[key] = values[values_key];
+        }
+      }
+      this.preventTooLargeGrowth();
+    }
+    this.triggerChange();
+  }
+
+  dygLabels = ['Date', 'pm10'];
+
+  private initDataValue = 0.042;
+  dygData = [
+    [new Date(new Date().valueOf() - 1), this.initDataValue],
+    [new Date(), this.initDataValue]
+  ];
+  cleanInitValues() {
+    if (
+      this.dygData.length === 2 &&
+      this.dygData[0][1] === this.initDataValue
+    ) {
+      this.dygData.shift();
+    }
+  }
+  preventTooLargeGrowth() {
+    while (this.dygData.length > 300) {
+      this.dygData.shift();
+    }
+  }
 
   calculateRealPM(valueArray: Array<number>) {
     const retArray = [
