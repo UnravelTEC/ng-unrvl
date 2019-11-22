289a522603b1a08e4538299aaf9227d459fae916
Michael Maier <michael.maier@unraveltec.com>
2019-11-22 09:06:48 +0100

    alphasense no2

---
+++ src/app/ut-apps/sensors/no2-b43f/no2-b43f.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no2-b43f',
+  templateUrl: './no2-b43f.component.html',
+  styleUrls: ['./no2-b43f.component.scss']
+})
+export class No2B43fComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/ut-apps/sensors/no2-b43f/no2-b43f.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { No2B43fRoutingModule } from './no2-b43f-routing.module';
+import { No2B43fComponent } from './no2-b43f.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    No2B43fRoutingModule
+  ],
+  declarations: [No2B43fComponent]
+})
+export class No2B43fModule { }
