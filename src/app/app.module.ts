b76b1cbfd75f6955ce36312818b42db5c337e5df
Michael Maier <michael.maier@unraveltec.com>
2020-03-01 09:13:03 +0100

    indoor climate i app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -238,6 +238,11 @@ const appRoutes: Routes = [
       './ut-apps/i/pressure/pressure.module#PressureModule'
   },
   {
+    path: 'Apps/I/IndoorClimate',
+    loadChildren:
+      './ut-apps/i/indoorclimate/indoorclimate.module#IndoorclimateModule'
+  },
+  {
     path: 'Apps/I/CPU',
     loadChildren:
       './ut-apps/i/coretemps/coretemps.module#CoretempsModule'
